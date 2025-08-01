447fd0a8f2dec4e8b06afb9a519a1b62617b7f64
zeki31 <ozeki.f.h.13@gmail.com>
2025-08-01 06:22:15 +0000

    initialization

---
+++ pip.sh
@@ -0,0 +1,4 @@
+# Install the pytorch version for your cuda version.
+pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
+pip install --upgrade setuptools cython wheel
+pip install -r requirements.txt
---
+++ pyenv.sh
@@ -0,0 +1,8 @@
+# Need to run in CUDA 11.8 environment
+
+pyenv local miniconda3-latest
+conda create -n Ex4DGS python=3.8 -y
+pyenv local miniconda3-latest/envs/Ex4DGS
+
+# pyenv local miniconda3-latest
+# conda env remove -n Ex4DGS -y
