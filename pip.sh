c237df7a060c6c9a9e0efa3877aeee5993732fd0
zeki31 <ozeki.f.h.13@gmail.com>
2025-08-01 06:22:53 +0000

    initialization

--- pip.sh
+++ pip.sh
@@ -2,3 +2,6 @@
 pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
 pip install --upgrade setuptools cython wheel
 pip install -r requirements.txt
+
+pip install pre-commit
+pre-commit install
